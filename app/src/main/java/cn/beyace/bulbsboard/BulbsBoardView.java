9aa464364c7209625e30e6175bbe75d6ad6142dd
HuangZilong <kirilenko1988@sina.com>
2017-05-08 11:09:16 +0800

    add library view

---
+++ app/src/main/java/cn/beyace/bulbsboard/BulbsBoardView.java
@@ -0,0 +1,24 @@
+package cn.beyace.bulbsboard;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by huangzilong on 2017/5/8.
+ */
+
+public class BulbsBoardView extends View {
+    public BulbsBoardView(Context context) {
+        super(context);
+    }
+
+    public BulbsBoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BulbsBoardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
