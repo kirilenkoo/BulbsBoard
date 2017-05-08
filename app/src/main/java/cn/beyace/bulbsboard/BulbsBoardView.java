11b0f5812dd9f83e8e6e5d3df1300d755fc9118d
HuangZilong <kirilenko1988@sina.com>
2017-05-08 16:49:42 +0800

    add simple byte arry input

--- app/src/main/java/cn/beyace/bulbsboard/BulbsBoardView.java
+++ app/src/main/java/cn/beyace/bulbsboard/BulbsBoardView.java
@@ -15,6 +15,7 @@ import android.view.View;
 
 public class BulbsBoardView extends View {
     byte[][] byteArrayData;
+    byte[] simpleByteArrayData;
     int bulbColor;
 
     public BulbsBoardView(Context context) {
@@ -42,11 +43,21 @@ public class BulbsBoardView extends View {
 
     public void setByteArrayData(byte[][] bytes){
         this.byteArrayData = bytes;
+        invalidate();
+    }
+
+    public void setSimpleByteArrayData(byte[] bytes){
+        this.simpleByteArrayData = bytes;
+        invalidate();
     }
 
     public void setBulbColor(int color){
         this.bulbColor = color;
     }
+
+    //TODO: add 1,2,3,4,5,6,7,8,9,0,a,b....
+    //TODO: make style fancy
+    //TODO: add shadow
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -80,7 +91,6 @@ public class BulbsBoardView extends View {
                         canvas.translate(bulbGap + bulbRadius, 0);
                     }else{
                         canvas.translate(bulbGap + bulbRadius*2, 0);
-
                     }
                     if(byteArrayData[i][j] != 0 ) {
                         canvas.drawCircle(0, 0, bulbRadius, lightBulbPaint);
@@ -90,6 +100,184 @@ public class BulbsBoardView extends View {
                 }
                 canvas.restore();
             }
+        }else if(simpleByteArrayData != null){
+            int bulbsColumnCount = 7;
+            int bulbGap = 10;
+            int bulbRadius = (canvas.getWidth()-bulbGap*(bulbsColumnCount+1))/bulbsColumnCount/2;
+            for(int i = 0; i<simpleByteArrayData.length; i++){
+                if(i == 0) {
+                    canvas.translate(0, bulbGap + bulbRadius);
+                }else{
+                    canvas.translate(0, bulbGap + bulbRadius*2);
+                }
+                canvas.save();
+                byte b = simpleByteArrayData[i];
+//                int mask = 1;
+                for(int j = 0; j<bulbsColumnCount; j++){
+                    if(j == 0) {
+                        canvas.translate(bulbGap + bulbRadius, 0);
+                    }else{
+                        canvas.translate(bulbGap + bulbRadius*2, 0);
+                    }
+                    int mask = 1;
+                    mask = mask << bulbsColumnCount-1-j;
+                    int tmp = b & mask;
+                    if(tmp>0){
+                        canvas.drawCircle(0, 0, bulbRadius, lightBulbPaint);
+                    }else{
+                        canvas.drawCircle(0, 0, bulbRadius, unLightBulbPaint);
+                    }
+                    Log.d("tmp", Integer.toBinaryString(tmp));
+                }
+                canvas.restore();
+            }
         }
+    }
+
+    public static byte[] parseInt2SimpleByteArray(int i){
+        byte[] sByte;
+        switch (i){
+            case 0:
+                sByte = new byte[]{
+                        0b1111111,
+                        0b1000001,
+                        0b1000001,
+                        0b1000001,
+                        0b1000001,
+                        0b1000001,
+                        0b1111111,
+                };
+                break;
+            case 1:
+                sByte = new byte[]{
+                        0b0001000,
+                        0b0001000,
+                        0b0001000,
+                        0b0001000,
+                        0b0001000,
+                        0b0001000,
+                        0b0001000,
+                };
+                break;
+            case 2:
+                sByte = new byte[]{
+                        0b1111111,
+                        0b0000001,
+                        0b0000001,
+                        0b1111111,
+                        0b1000000,
+                        0b1000000,
+                        0b1111111,
+                };
+                break;
+            case 3:
+                sByte = new byte[]{
+                        0b1111111,
+                        0b0000001,
+                        0b0000001,
+                        0b1111111,
+                        0b0000001,
+                        0b0000001,
+                        0b1111111,
+                };
+                break;
+            case 4:
+                sByte = new byte[]{
+                        0b1001000,
+                        0b1001000,
+                        0b1001000,
+                        0b1111111,
+                        0b0001000,
+                        0b0001000,
+                        0b0001000,
+                };
+                break;
+            case 5:
+                sByte = new byte[]{
+                        0b1111111,
+                        0b1000000,
+                        0b1000000,
+                        0b1111111,
+                        0b0000001,
+                        0b0000001,
+                        0b1111111,
+                };
+                break;
+            default:
+                sByte = new byte[]{
+                        0b0000000,
+                        0b0000000,
+                        0b0000000,
+                        0b0000000,
+                        0b0000000,
+                        0b0000000,
+                        0b0000000,
+                };
+                break;
+        }
+        return sByte;
+    }
+
+    public static byte[][] parseInt2ByteArray(int i){
+        byte[][] bytes;
+        switch (i){
+            case 0:
+                bytes = new byte[][]{
+                        {1,1,1,1,1},
+                        {1,0,0,0,1},
+                        {1,0,0,0,1},
+                        {1,0,0,0,1},
+                        {1,1,1,1,1},
+                };
+                break;
+            case 1:
+                bytes = new byte[][]{
+                        {0,0,1,0,0},
+                        {0,0,1,0,0},
+                        {0,0,1,0,0},
+                        {0,0,1,0,0},
+                        {0,0,1,0,0},
+                };
+                break;
+            case 2:
+                bytes = new byte[][]{
+                        {1,1,1,1,1},
+                        {0,0,0,0,1},
+                        {1,1,1,1,1},
+                        {1,0,0,0,0},
+                        {1,1,1,1,1},
+                };
+                break;
+            case 3:
+                bytes = new byte[][]{
+                        {1,1,1,1,1},
+                        {0,0,0,0,1},
+                        {1,1,1,1,1},
+                        {0,0,0,0,1},
+                        {1,1,1,1,1},
+                };
+                break;
+            case 4:
+                bytes = new byte[][]{
+                        {1,0,0,1,0},
+                        {1,0,0,1,0},
+                        {1,1,1,1,1},
+                        {0,0,0,1,0},
+                        {0,0,0,1,0},
+                };
+                break;
+            case 5:
+                bytes = new byte[][]{
+                        {1,1,1,1,1},
+                        {1,0,0,0,0},
+                        {1,1,1,1,1},
+                        {0,0,0,0,1},
+                        {1,1,1,1,1},
+                };
+                break;
+            default:
+                bytes = new byte[][]{};
+        }
+        return bytes;
     }
 }
--- app/src/main/java/cn/beyace/bulbsboard/MainActivity.java
+++ app/src/main/java/cn/beyace/bulbsboard/MainActivity.java
@@ -1,27 +1,38 @@
 package cn.beyace.bulbsboard;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     BulbsBoardView bulbsBoardView;
     byte[][] byteData;
+    byte[] simpleByteData;
+    long l = 0x84877578;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bulbsBoardView = (BulbsBoardView) findViewById(R.id.bulbsBoardView);
-        byteData = new byte[][]{
-                new byte[]{
-                        0,1,1,1,0
-                },
-                new byte[]{
-                        0,0,1,0,0
-                },
-                new byte[]{
-                        0,1,1,1,0
-                }
+//        bulbsBoardView.setByteArrayData(BulbsBoardView.parseInt2ByteArray(5));
+//        bulbsBoardView.setSimpleByteArrayData(BulbsBoardView.parseInt2SimpleByteArray(3));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CountDownTimer countDownTimer = new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                bulbsBoardView.setSimpleByteArrayData(BulbsBoardView.parseInt2SimpleByteArray((int) (millisUntilFinished/1000)));
+            }
+
+            @Override
+            public void onFinish() {
+                bulbsBoardView.setSimpleByteArrayData(BulbsBoardView.parseInt2SimpleByteArray(0));
+            }
         };
-        bulbsBoardView.setByteArrayData(byteData);
+        countDownTimer.start();
+
     }
 }
